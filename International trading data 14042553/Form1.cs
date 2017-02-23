3ec45bc6c99457b1cafe3f6d25ffaf00f5e1102d
Sam <Sam@DESKTOP-A03NMH1>
2017-02-23 16:12:11 +0000

    Add project files.

---
+++ International trading data 14042553/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace International_trading_data_14042553
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
